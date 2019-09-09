489e27d0980b4c0037a12054a54d2e1975904a50
James Wu <jameswu62@yahoo.co.nz>
2019-09-09 17:22:09 +1200

    edit main.c, add function prefix

--- main.c
+++ main.c
@@ -2,9 +2,9 @@
 #include <stdint.h>
 #include "cli.h"
 
-void test1_func(char*, int, int);
-void test2_func(char*, int, int);
-void test3_func(char*, int, int);
+void l1_test1_func(char*, int, int);        // level 1 command, test1
+void l1_test2_func(char*, int, int);        // level 1 command, test2
+void l1_test3_func(char*, int, int);        // level 1 command, test3
 
 int main(int argc, char *argv[])
 {
@@ -15,13 +15,13 @@ int main(int argc, char *argv[])
     cli_param_init(cmd_L1, sizeof(cmd_L1)/sizeof(cmd_L1[0]));
     cli_param_init(cmd_L2, sizeof(cmd_L2)/sizeof(cmd_L2[0]));
     
-    cmd_L1[0].callback = test1_func;
+    cmd_L1[0].callback = l1_test1_func;
     cmd_L1[0].cmd = "cmd1";
 
-    cmd_L1[1].callback = test2_func;
+    cmd_L1[1].callback = l1_test2_func;
     cmd_L1[1].cmd = "cmd2";
 
-    cmd_L1[2].callback = test3_func;
+    cmd_L1[2].callback = l1_test3_func;
     cmd_L1[2].cmd = "cmd3";
     
     cli_print(cmd_L1, sizeof(cmd_L1)/sizeof(cmd_L1[0]));
@@ -44,14 +44,14 @@ int main(int argc, char *argv[])
     return 0;
 }
 
-void test1_func(char* input, int arg1, int arg2){
-    printf("test_1\n");
+void l1_test1_func(char* input, int arg1, int arg2){
+    printf("L1_test_1\n");
 }
 
-void test2_func(char* input, int arg1, int arg2){
-    printf("test_2\n");
+void l1_test2_func(char* input, int arg1, int arg2){
+    printf("L1_test_2\n");
 }
 
-void test3_func(char* input, int arg1, int arg2){
-    printf("test_3\n");
+void l1_test3_func(char* input, int arg1, int arg2){
+    printf("L1_test_3\n");
 }
